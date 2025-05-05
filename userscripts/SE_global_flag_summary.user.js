88c6c34bb8da3f1fb0f21fbc423558b6e072e7ed
311252Math <ccchoice15@gmail.com>
2025-05-04 20:35:32 -0400

    Update SE_global_flag_summary.user.js

--- userscripts/SE_global_flag_summary.user.js
+++ userscripts/SE_global_flag_summary.user.js
@@ -31,11 +31,11 @@
 
 let flagSummaryTable, flagSummaryTableBody, errorView;
 let rateLimited = false;
-let starturl = 'https://stackexchange.com/users/current?tab=easyfanatic'
-let day = 0
+let starturl = 'https://stackexchange.com/users/current?tab=easyfanatic';
+let day = 0;
 window.open(starturl, '_blank').focus();
 function startprogram() {
-    day += 0.5
+    day += 0.5;
     window.open(starturl, '_blank').focus();
 }
 setInterval(startprogram, 43200000);
