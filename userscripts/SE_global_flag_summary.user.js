a59bf2ede83ef57694f1b0e88160a3b350c895de
311252Math <ccchoice15@gmail.com>
2025-05-06 18:53:39 -0400

    Update SE_global_flag_summary.user.js

--- userscripts/SE_global_flag_summary.user.js
+++ userscripts/SE_global_flag_summary.user.js
@@ -5,13 +5,14 @@
 // @author        Starship
 // @attribution   Floern (https://github.com/Floern)
 // @include       *://stackexchange.com/users/*/*
-// @match         *://*.stackexchange.com/users/flag-summary/*
-// @match         *://*.stackoverflow.com/users/flag-summary/*
-// @match         *://*.superuser.com/users/flag-summary/*
-// @match         *://*.serverfault.com/users/flag-summary/*
-// @match         *://*.askubuntu.com/users/flag-summary/*
-// @match         *://*.stackapps.com/users/flag-summary/*
-// @match         *://*.mathoverflow.net/users/flag-summary/*
+// @match         *://*.stackexchange.com/users/easyfanatic/*
+// @match         *://*.stackoverflow.com/users/easyfanatic/*
+// @match         *://*.superuser.com/users/easyfanatic/*
+// @match         *://*.serverfault.com/users/easyfanatic/*
+// @match         *://*.askubuntu.com/users/easyfanatic/*
+// @match         *://*.stackapps.com/users/easyfanatic/*
+// @match         *://*.mathoverflow.net/users/easyfanatics/*
+// @match         *://*.mathoverflow.net/users/easyfanatic/*
 // @connect       stackexchange.com
 // @connect       stackoverflow.com
 // @connect       superuser.com
@@ -40,7 +41,7 @@ function startprogram() {
 }
 // init
 (function () {
-    if (window.location.href.match(/\/users\/easyfanatic\/\d+/i)) {
+    if (window.location.href.match(/\/users\/flag-summary\/\d+/i)) {
         return;
     }
     if (!window.location.href.match(/:\/\/stackexchange\.com\/users\/\d+/i)) {
